62cb65712d47c7be635352763c7b5a097c62d6ad
Anthony <antmarasia@gmail.com>
2020-12-12 15:39:25 -0500

    Added segue to DetailViewController. Added formatting.

--- RockTracks/DetailViewController.swift
+++ RockTracks/DetailViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    var track: Track!
 
     @IBOutlet weak var albumArtImageView: UIImageView!
     @IBOutlet weak var trackNameLabel: UILabel!
@@ -20,7 +22,46 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.trackNameLabel.text = track.name
+        self.artistNameLabel.text = track.artist
+        
+        formatPrice()
+        
+        formatReleaseDate()
+        
+        formatDuration()
+        
+    }
+    
+    func formatPrice() {
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale.current
+        
+        if let priceString = currencyFormatter.string(from: NSNumber(value: track.price)) {
+            self.trackPriceLabel.text = priceString
+        }
+    }
+    
+    func formatReleaseDate() {
+        //"releaseDate": "2011-01-24T08:00:00Z"
+        // yyyy-MM-ddTHH:mm:ssZ
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+
+        if let date = isoFormatter.date(from: track.releaseDate) {
+            self.releaseDateLabel.text = dateFormatter.string(from: date)
+        }
+    }
+    
+    func formatDuration() {
+        let durationInSeconds = track.duration / 1000
+        let seconds = durationInSeconds % 60
+        let minutes = (durationInSeconds / 60) % 60
+        self.durationLabel.text = String(format: "%0.2d:%0.2d",minutes,seconds)
     }
     
     @IBAction func moreDetailButtonPressed(_ sender: Any) {
--- RockTracks/TableViewController.swift
+++ RockTracks/TableViewController.swift
@@ -50,52 +50,31 @@ class TableViewController: UITableViewController {
         return 120
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let track = tracks[indexPath.row]
+        self.performSegue(withIdentifier: "showDetailVC", sender: track)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "showDetailVC" {
+            if let detailVC = segue.destination as? DetailViewController {
+                if let track = sender as? Track {
+                    detailVC.track = track
+                }
+            }
+        }
     }
-    */
     
-    // MARK: -
+    // MARK: - My Functions
     
     func getTracks() {
         NetworkManager.getTrackSearchResults { (tracks) in
