d808d290d7bc08574298b5837873da771433edb2
Anthony <antmarasia@gmail.com>
2020-12-12 13:27:49 -0500

    Added screen 1 TableViewController. Designed TrackTableViewCell

---
+++ RockTracks/Views/TrackTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TrackTableViewCell.swift
+//  RockTracks
+//
+//  Created by Anthony on 12/12/20.
+//
+
+import UIKit
+
+class TrackTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var albumArtImageView: UIImageView!
+    @IBOutlet weak var tackNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
